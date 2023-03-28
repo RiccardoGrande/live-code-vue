b556281e2fe98d5d6e532b9ff424f003f7a8ce2b
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-28 12:39:29 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue
+
+createApp({
+    /* option object */
+  data() {
+    return {
+      message: 'Hello Vue!',
+      contentStyle: 'content'
+    }
+  }
+}).mount('#app')
+
+
+
+
